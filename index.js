9163f936a84b82d27ae09818aa76cbbc34de07aa
Jarret Nachtigal <jarretn1224@gmail.com>
2021-11-08 13:43:58 -0500

    moment js, webpack-cli

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("it hath become gamer time");
+/* global moment */
+var moment = require("moment");
+
+let gamerTime = moment().format("MMMM Do YYYY, h:mm:ss a");
+console.log("gamer time = " + gamerTime); // its always gamer time
